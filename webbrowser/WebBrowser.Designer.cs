9da274d52750c1dd0456da7f9566035fb1413e12
mosshkina <113132709+mosshkina@users.noreply.github.com>
2023-04-14 19:27:49 +0300

    Добавьте файлы проекта.

---
+++ webbrowser/WebBrowser.Designer.cs
@@ -0,0 +1,167 @@
+﻿namespace webbrowser
+{
+    partial class WebBrowser
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(WebBrowser));
+            this.toolStrip1 = new System.Windows.Forms.ToolStrip();
+            this.BackButton = new System.Windows.Forms.ToolStripButton();
+            this.ForwardButton = new System.Windows.Forms.ToolStripButton();
+            this.RefreshButton = new System.Windows.Forms.ToolStripButton();
+            this.toolStripTextBox1 = new System.Windows.Forms.ToolStripTextBox();
+            this.GoButton = new System.Windows.Forms.ToolStripButton();
+            this.PlusButton = new System.Windows.Forms.ToolStripButton();
+            this.DeleteButton = new System.Windows.Forms.ToolStripButton();
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.toolStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // toolStrip1
+            // 
+            this.toolStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.toolStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.BackButton,
+            this.ForwardButton,
+            this.RefreshButton,
+            this.toolStripTextBox1,
+            this.GoButton,
+            this.PlusButton,
+            this.DeleteButton});
+            this.toolStrip1.Location = new System.Drawing.Point(0, 0);
+            this.toolStrip1.Name = "toolStrip1";
+            this.toolStrip1.Size = new System.Drawing.Size(1252, 31);
+            this.toolStrip1.TabIndex = 0;
+            this.toolStrip1.Text = "toolStrip1";
+            // 
+            // BackButton
+            // 
+            this.BackButton.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.BackButton.Image = ((System.Drawing.Image)(resources.GetObject("BackButton.Image")));
+            this.BackButton.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.BackButton.Name = "BackButton";
+            this.BackButton.Size = new System.Drawing.Size(29, 28);
+            this.BackButton.Text = "Назад";
+            this.BackButton.Click += new System.EventHandler(this.BackButton_Click);
+            // 
+            // ForwardButton
+            // 
+            this.ForwardButton.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.ForwardButton.Image = ((System.Drawing.Image)(resources.GetObject("ForwardButton.Image")));
+            this.ForwardButton.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.ForwardButton.Name = "ForwardButton";
+            this.ForwardButton.Size = new System.Drawing.Size(29, 28);
+            this.ForwardButton.Text = "Вперед";
+            this.ForwardButton.Click += new System.EventHandler(this.ForwardButton_Click);
+            // 
+            // RefreshButton
+            // 
+            this.RefreshButton.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.RefreshButton.Image = ((System.Drawing.Image)(resources.GetObject("RefreshButton.Image")));
+            this.RefreshButton.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.RefreshButton.Name = "RefreshButton";
+            this.RefreshButton.Size = new System.Drawing.Size(29, 28);
+            this.RefreshButton.Text = "Обновить";
+            this.RefreshButton.Click += new System.EventHandler(this.RefreshButton_Click);
+            // 
+            // toolStripTextBox1
+            // 
+            this.toolStripTextBox1.Font = new System.Drawing.Font("Segoe UI", 9F);
+            this.toolStripTextBox1.Name = "toolStripTextBox1";
+            this.toolStripTextBox1.Size = new System.Drawing.Size(800, 31);
+            this.toolStripTextBox1.KeyUp += new System.Windows.Forms.KeyEventHandler(this.toolStripTextBox1_KeyUp);
+            // 
+            // GoButton
+            // 
+            this.GoButton.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.GoButton.Image = ((System.Drawing.Image)(resources.GetObject("GoButton.Image")));
+            this.GoButton.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.GoButton.Name = "GoButton";
+            this.GoButton.Size = new System.Drawing.Size(29, 28);
+            this.GoButton.Text = "Поиск";
+            this.GoButton.Click += new System.EventHandler(this.GoButton_Click);
+            // 
+            // PlusButton
+            // 
+            this.PlusButton.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.PlusButton.Image = ((System.Drawing.Image)(resources.GetObject("PlusButton.Image")));
+            this.PlusButton.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.PlusButton.Name = "PlusButton";
+            this.PlusButton.Size = new System.Drawing.Size(29, 28);
+            this.PlusButton.Text = "Новая вкладка";
+            this.PlusButton.Click += new System.EventHandler(this.PlusButton_Click);
+            // 
+            // DeleteButton
+            // 
+            this.DeleteButton.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.DeleteButton.Image = ((System.Drawing.Image)(resources.GetObject("DeleteButton.Image")));
+            this.DeleteButton.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.DeleteButton.Name = "DeleteButton";
+            this.DeleteButton.Size = new System.Drawing.Size(29, 28);
+            this.DeleteButton.Text = "Удалить вкладку";
+            this.DeleteButton.Click += new System.EventHandler(this.DeleteButton_Click);
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tabControl1.Location = new System.Drawing.Point(0, 31);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(1252, 547);
+            this.tabControl1.TabIndex = 1;
+            // 
+            // WebBrowser
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1252, 578);
+            this.Controls.Add(this.tabControl1);
+            this.Controls.Add(this.toolStrip1);
+            this.Name = "WebBrowser";
+            this.Text = "Браузер";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.toolStrip1.ResumeLayout(false);
+            this.toolStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ToolStrip toolStrip1;
+        private System.Windows.Forms.ToolStripButton BackButton;
+        private System.Windows.Forms.ToolStripButton ForwardButton;
+        private System.Windows.Forms.ToolStripButton RefreshButton;
+        private System.Windows.Forms.ToolStripTextBox toolStripTextBox1;
+        private System.Windows.Forms.ToolStripButton GoButton;
+        private System.Windows.Forms.ToolStripButton PlusButton;
+        private System.Windows.Forms.ToolStripButton DeleteButton;
+        private System.Windows.Forms.TabControl tabControl1;
+    }
+}
+
---
+++ webbrowser/WebBrowser.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Security.Policy;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml;
+
+namespace webbrowser
+{
+    
+    public partial class WebBrowser : Form
+    {
+        int i = 0;
+        public System.Windows.Forms.WebBrowser wp = new System.Windows.Forms.WebBrowser();
+        public WebBrowser()
+        {
+            InitializeComponent();
+            CenterToScreen();
+        }
+
+        private void PlusButton_Click(object sender, EventArgs e)
+        {
+            System.Windows.Forms.WebBrowser webBrowser = new System.Windows.Forms.WebBrowser();   
+            webBrowser.Visible = true;
+            webBrowser.ScriptErrorsSuppressed= true;
+            webBrowser.Dock= DockStyle.Fill;
+            webBrowser.DocumentCompleted += WebBrowser_DocumentCompleted;
+            tabControl1.TabPages.Add("New Page");
+            tabControl1.SelectTab(i);
+            tabControl1.SelectedTab.Controls.Add(webBrowser);
+            i += 1;
+        }
+
+        private void WebBrowser_DocumentCompleted(object sender, WebBrowserDocumentCompletedEventArgs e)
+        {
+            tabControl1.SelectedTab.Text = ((System.Windows.Forms.WebBrowser)tabControl1.SelectedTab.Controls[0]).DocumentTitle;
+        }
+
+        private void GoButton_Click(object sender, EventArgs e)
+        {
+            if(toolStripTextBox1.Text != null)
+            {
+                ((System.Windows.Forms.WebBrowser)tabControl1.SelectedTab.Controls[0]).Navigate(toolStripTextBox1.Text);
+            }
+            else
+            {
+
+            }
+        }
+
+        private void BackButton_Click(object sender, EventArgs e)
+        {
+            ((System.Windows.Forms.WebBrowser)tabControl1.SelectedTab.Controls[0]).GoBack();
+        }
+
+        private void ForwardButton_Click(object sender, EventArgs e)
+        {
+            ((System.Windows.Forms.WebBrowser)tabControl1.SelectedTab.Controls[0]).GoForward();
+        }
+
+        private void RefreshButton_Click(object sender, EventArgs e)
+        {
+            ((System.Windows.Forms.WebBrowser)tabControl1.SelectedTab.Controls[0]).Refresh();
+        }
+
+        private void DeleteButton_Click(object sender, EventArgs e)
+        {
+            if (tabControl1.TabPages.Count > 1)
+            { 
+                tabControl1.TabPages.RemoveAt(tabControl1.SelectedIndex);
+                tabControl1.SelectTab(tabControl1.TabPages.Count - 1);
+                i -=1;
+            }
+            else
+                Application.Exit();
+        }
+
+        private void toolStripTextBox1_KeyUp(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter)
+            {
+                ((System.Windows.Forms.WebBrowser)tabControl1.SelectedTab.Controls[0]).Navigate(toolStripTextBox1.Text);
+            }
+        }
+        public void ToListHis(List<string> list, XmlElement doc) // добавление информации при запуске прграммы
+        {
+            foreach (XmlElement elem in doc)
+            {
+                list.Add($"{elem.ChildNodes[0].InnerText} {elem.ChildNodes[1].InnerText}");
+            }
+        }
+       
+        
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
